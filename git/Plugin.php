5b376b0d0ebf6c62e7ef56a0178702869a9ae700
Boudewijn Schoon <boudewijn@zicht.nl>
2019-04-04 12:27:52 +0200

    Use descriptive version instead of hash

--- git/Plugin.php
+++ git/Plugin.php
@@ -30,6 +30,7 @@ class Plugin extends BasePlugin
             ->children()
                 ->arrayNode('vcs')
                     ->children()
+                        ->booleanNode('descriptive_version')->defaultValue(true)->end()
                         ->scalarNode('url')->end()
                         ->arrayNode('export')
                             ->children()
@@ -62,16 +63,26 @@ class Plugin extends BasePlugin
         $container->method(
             array('versionof'),
             function($container, $dir) {
-                $info = $container->helperExec('cd ' . escapeshellarg($dir) . ' && git log $(git rev-parse --abbrev-ref HEAD) -1');
 
-                if (!$info && is_file($revFile = ($dir . '/' . $container->resolve(array('vcs', 'export', 'revfile'))))) {
-                    $info = file_get_contents($revFile);
-                }
-
-                if ($info) {
-                    return $container->call($container->resolve(array('vcs', 'versionid')), $info);
+                $isDescriptive = $container->resolve(array('vcs', 'descriptive_version'));
+                if ($isDescriptive) {
+                    $output = $container->helperExec('cd ' . escapeshellarg($dir) . ' && git describe --always --match "*.*.*" --tags HEAD');
+                    if (preg_match('/(?P<version>[^\s]+)/im', $output, $m)) {
+                        return $m['version'];
+                    }
+                    throw new \RuntimeException("Could not find descriptive version in:\n<comment>" . $output . "</comment>");
                 } else {
-                    return null;
+                    $info = $container->helperExec('cd ' . escapeshellarg($dir) . ' && git log $(git rev-parse --abbrev-ref HEAD) -1');
+
+                    if (!$info && is_file($revFile = ($dir . '/' . $container->resolve(array('vcs', 'export', 'revfile'))))) {
+                        $info = file_get_contents($revFile);
+                    }
+
+                    if ($info) {
+                        return $container->call($container->resolve(array('vcs', 'versionid')), $info);
+                    } else {
+                        return null;
+                    }
                 }
             }
         );
